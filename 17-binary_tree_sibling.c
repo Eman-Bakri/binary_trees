27039712c133af39e447a02b8845bdcff2e4c9e0
Eman-Bakri <eman1996-sy@hotmail.com>
2023-11-29 02:44:04 -0800

    sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -17,8 +17,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node->parent->right == node)
 		return (node->parent->left);
 
-	if (node->parent->left == node)
-		return (node->parent->right);
-
-	return (NULL);
+	return (node->parent->right);
 }
