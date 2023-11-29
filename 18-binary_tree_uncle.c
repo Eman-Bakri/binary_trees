7fea8e09a8a75295a3ddaea2908231e8880d55e3
Eman-Bakri <eman1996-sy@hotmail.com>
2023-11-29 01:50:15 -0800

    fixing betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,9 +10,11 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 
 	if (node == NULL ||
-	    node->parent->parent == NULL ||
-	    node->parent == NULL)
-			return (NULL);
+	node->parent->parent == NULL ||
+	node->parent == NULL) {
+		return (NULL);
+	}
+
 
 	if (node->parent->parent->right == node->parent)
 		return (node->parent->parent->left);
