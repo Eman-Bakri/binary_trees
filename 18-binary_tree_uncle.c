0efdf2fb88c1f73967bbcce64529a7940517a16b
Eman-Bakri <eman1996-sy@hotmail.com>
2023-11-29 03:40:35 -0800

    uncle node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,14 +8,16 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!(node->parent->parent) || !(node->parent))
-		return (NULL);
 
 	if (node == NULL)
 		return (NULL);
 
-	if ((node->parent->parent->left) == (node->parent))
-		return (node->parent->parent->right);
-	else
+	if (!(node->parent->parent) || !(node->parent))
+		return (NULL);
+
+
+	if ((node->parent->parent->right) == (node->parent))
 		return (node->parent->parent->left);
+	else
+		return (node->parent->parent->right);
 }
