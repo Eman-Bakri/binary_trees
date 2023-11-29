9d1beecdb273ff916874d58059f0eb03cc09ec69
Eman-Bakri <eman1996-sy@hotmail.com>
2023-11-29 02:19:34 -0800

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,13 +8,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t right_ht = 0;
-	size_t left_ht = 0;
 
-	if (tree != NULL)
+	if (tree)
 	{
-		right_ht = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		left_ht = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		size_t right_ht = 0;
+		size_t left_ht = 0;
+
+		right_ht = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		left_ht = tree->left ? 1 + binary_tree_height(tree->left) : 1;
 
 		return ((left_ht > right_ht) ? left_ht : right_ht);
 	}
